d3f89f570920aaefffff6ecee8f3fd01beec716d
kwaw kumi <kwawkumi9@gmail.com>
2023-12-04 09:20:11 +0000

    Initial Commit

---
+++ stateobject/stateobjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  stateobjectApp.swift
+//  stateobject
+//
+//  Created by kwaw-kumi on 04/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct stateobjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
